4d9d9ae34d180b7e6558b076f8a7b6db78bef275
Panca Purnama Adhiputra <118573012+pancapurnama@users.noreply.github.com>
2023-10-06 09:12:53 +0700

    Add files via upload
    Tugas Praktikum 2

---
+++ praktikum_php8/login.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+$hashed_password = md5("rahasia");
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    if ($_POST['username'] === "UserSatu" && md5($_POST['password']) === $hashed_password) {
+        $_SESSION['loggedin'] = true;
+        setcookie("user", "UserSatu", time() + (86400 * 30), "/");
+        header("Location: jadwal_kereta.php");
+        exit;
+    } else {
+        $error_message = "Username atau Password salah!";
+    }
+}
+
+$hashed_password = md5("adhptr26");
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    if ($_POST['username'] === "pancadundun" && md5($_POST['password']) === $hashed_password) {
+        $_SESSION['loggedin'] = true;
+        setcookie("user", "pancadundun",time() + (86400 * 30), "/");
+        header("location: jadwal_kereta.php");
+        exit;
+    } else {
+        $error_message = "Username atau Password Salah";
+    }
+}
+
+include "header.php";
+?>
+
+<form action="login.php" method="post">
+    Username: <input type="text" name="username" required><br>
+    Password: <input type="password" name="password" required><br>
+    <input type="submit" value="Login">
+</form>
+
+<?php
+if (isset($error_message)) {
+    echo "<p style='color:red;'>$error_message</p>";
+}
+include "footer.php";
